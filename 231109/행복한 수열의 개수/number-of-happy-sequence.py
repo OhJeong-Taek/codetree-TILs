ee6b0c65302dcf39acc728560bb947e7b85d39a1
OhJeong-Taek <ojt0128@gmail.com>
2023-11-09 09:29:51 +0900

    Title: 행복한 수열의 개수, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231109/행복한 수열의 개수/number-of-happy-sequence.py
+++ 231109/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,37 +1,28 @@
 n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
+grid = [list(map(int, input().split())) for _ in range(n)]
+seq = [0 for _ in range(n)]
 
+def is_happy_sequence():
+    consequtive, max_ccnt = 1, 1
+    for i in range(1,n):
+        if seq[i-1] == seq[i]:
+            consequtive += 1
+        else:
+            consequtive = 1
+        max_ccnt = max(max_ccnt, consequtive)
+    return max_ccnt >= m
 
 answer = 0
 #row
 for i in range(n):
-    consequtive = 0
-    maxConsequtive = 0
-    bef = board[i][0]
-    for j in range(n):
-        cur = board[i][j]
-        if bef == cur:
-            consequtive += 1
-        else:
-            consequtive = 1
-        maxConsequtive = max(maxConsequtive, consequtive)
-        bef = cur
-    if maxConsequtive >= m:
+    seq = grid[i][:]
+    if is_happy_sequence():
         answer += 1
 
 #column
 for i in range(n):
-    consequtive = 0
-    maxConsequtive = 0
-    bef = board[0][i]
     for j in range(n):
-        cur = board[j][i]
-        if bef == cur:
-            consequtive += 1
-        else:
-            consequtive = 1
-        maxConsequtive = max(maxConsequtive, consequtive)
-        bef = cur
-    if maxConsequtive >= m:
+        seq[i] = grid[j][i]
+    if is_happy_sequence():
         answer += 1
 print(answer)
