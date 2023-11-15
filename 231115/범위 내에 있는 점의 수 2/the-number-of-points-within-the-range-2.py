3bc1a5ad0d820a0576d7628ed1850bc529ac665b
OhJeong-Taek <ojt0128@gmail.com>
2023-11-15 15:36:55 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 537ms, Memory: 49MB, Status: Passed - CodeTree

--- 231115/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
+++ 231115/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.py
@@ -1,15 +1,15 @@
 N, Q = map(int, input().split())
-POS_LEN = 1000000 + 1
-arr = [0] * (POS_LEN)
-prefix_sum = [0] * (POS_LEN)
+POS_LEN = 1000000
+arr = [0] * (POS_LEN + 1)
+prefix_sum = [0] * (POS_LEN + 1)
 
 for val in list(map(int, input().split())):
     arr[val] = 1
 
 
-for i in range(1,POS_LEN):
+for i in range(1, POS_LEN + 1):
     prefix_sum[i] = prefix_sum[i-1] + arr[i]
 
 for _ in range(Q):
     s, e = map(int, input().split())
-    print(prefix_sum[e] - prefix_sum[s-1])
+    print(prefix_sum[e] - prefix_sum[s-1 if s>=1 else 0])
