0a19cb2fa7ad35e64e359f7ddfd3e5dda6ae09c9
OhJeong-Taek <ojt0128@gmail.com>
2023-11-15 12:01:58 +0900

    Title: 연속한 K개의 숫자, Time: 146ms, Memory: 26MB, Status: Passed - CodeTree

--- 231115/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 231115/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -2,14 +2,18 @@ import sys
 
 N, K, B = map(int, input().split())
 arr = [0]*(N+1) #arr[0~ N]
+prefix_sum = [0]*(N+1)
 
 for _ in range(B):
     val = int(input())
     arr[val] = 1
 
+for i in range(1, N+1):
+    prefix_sum[i] = prefix_sum[i-1] + arr[i]
+
 ans = sys.maxsize
 for i in range(1, N-K+2): #10 6
-    ans = min(ans, sum(arr[i:i+K]))    
-#On = (KN)
+    #ans = min(ans, sum(arr[i:i+K]))    #O(n) = (KN)
+    ans = min(ans, prefix_sum[i+K-1] - prefix_sum[i-1])
 
 print(ans)
